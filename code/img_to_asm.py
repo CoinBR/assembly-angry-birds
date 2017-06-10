a12238251a81fbcab317fe5a26981da8da4bad94
Pedro CoinBR Duarte <coinbr@gmail.com>
2017-06-09 23:48:03 -0300

    Make img_to_asm convert all images in 'imgs/' folder

--- code/img_to_asm.py
+++ code/img_to_asm.py
@@ -1,3 +1,6 @@
+from os import listdir
+from os.path import isfile, join, abspath, realpath, split, splitext, dirname
+
 from PIL import Image
 
 screen_size = (512, 256, )
@@ -6,37 +9,57 @@ pixel_size = 2
 screen_dimensions = (screen_size[0] // pixel_size,
                      screen_size[1] // pixel_size, )
 mem_start = 268500992  # lui 0x1001
-base_image_path = 'image.jpg'
-output_file = 'code.asm'
+root_path = dirname(realpath(__file__))
+imgs_path = join(root_path, 'imgs/')
+output_path = join(root_path, 'assembly/')
 
 
 def rgb_to_hex(rgb_tuple):
     return "0x{0:02x}{1:02x}{2:02x}".format(*rgb_tuple)
 
 
-def write_assembly(pixels):
+def convert_img_path_to_asm_path(img_path):
+    path, filename = split(img_path)
+    filename = splitext(filename)[0]
 
-    def gen_draw_pixel_code(pixel_address_tuple):
-        return """
-            addi $10, $0, {}
-            sw $10, {}($0)
-        """.format(*pixel_address_tuple)
-
-    def write_file(strings):
-        text_file = open(output_file, "w")
-        for s in strings:
-            text_file.write(s)
-        text_file.close()
-
-    args = ((pixels[i], mem_start + i * 4) for i in range(len(pixels)))
-    pixels_code = map(gen_draw_pixel_code, args)
-    write_file(pixels_code)
+    newfilename = '{}.asm'.format(filename)
+    return join(output_path, newfilename)
 
 
-img = Image.open(base_image_path).resize(screen_dimensions).convert('RGB')
-all_cordinates = [(w, h, ) for h in range(screen_dimensions[1])
-                  for w in range(screen_dimensions[0])]
+def process_image(img, out_path):
 
-rgb_pixels = map(img.getpixel, all_cordinates)
-pixels = map(rgb_to_hex, rgb_pixels)
-write_assembly(list(pixels))
+    def write_assembly(pixels):
+
+        def gen_draw_pixel_code(pixel_address_tuple):
+            return """
+                addi $10, $0, {}
+                sw $10, {}($0)
+            """.format(*pixel_address_tuple)
+
+        def write_file(strings):
+            text_file = open(out_path, "w")
+            for s in strings:
+                text_file.write(s)
+            text_file.close()
+
+        args = ((pixels[i], mem_start + i * 4) for i in range(len(pixels)))
+        pixels_code = map(gen_draw_pixel_code, args)
+        write_file(pixels_code)
+
+    all_cordinates = [(w, h, ) for h in range(screen_dimensions[1])
+                      for w in range(screen_dimensions[0])]
+
+    rgb_pixels = map(img.getpixel, all_cordinates)
+    pixels = map(rgb_to_hex, rgb_pixels)
+    write_assembly(list(pixels))
+
+
+imgs_paths = (join(imgs_path, f) for f in listdir(imgs_path)
+              if isfile(join(imgs_path, f)))
+
+imgs = ((Image.open(path).resize(screen_dimensions).convert('RGB'),
+         convert_img_path_to_asm_path(path), )
+        for path in imgs_paths)
+
+for img in imgs:
+    process_image(*img)
